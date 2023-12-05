9ec0041c199b61a1e21543b9647e3e60b15e6e0a
Zachary <zachary@Poopbuttcrapnutt.attlocal.net>
2023-12-05 13:17:13 -0800

    maybe you should move the function out of the comment you dumb fuck

--- Exercism/python/currency-exchange/exchange.py
+++ Exercism/python/currency-exchange/exchange.py
@@ -1,6 +1,7 @@
 def exchange_money(budget, exchange_rate):
-    """
+    return float(budget / exchange_rate)
 
+    """
     :param budget: float - amount of money you are planning to exchange.
     :param exchange_rate: float - unit value of the foreign currency.
     :return: float - exchanged value of the foreign currency you can receive.
